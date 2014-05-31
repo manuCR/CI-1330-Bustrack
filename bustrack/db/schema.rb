93a3a9c0ecb3294e1f89bbede277feae74a72e4b
b04539 <andony91@gmail.com>
2014-05-30 22:28:26 -0600

    buscar to y from

--- bustrack/db/schema.rb
+++ bustrack/db/schema.rb
@@ -11,9 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140518041051) do
+ActiveRecord::Schema.define(version: 20140530102221) do
 
   create_table "mapas", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "paradas", force: true do |t|
+    t.string   "nombre"
+    t.boolean  "techo"
+    t.float    "latitid"
+    t.float    "longuitud"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ruta", force: true do |t|
+    t.string   "nombre"
+    t.float    "Inicio_Latitud"
+    t.float    "Inicio_Longuitud"
+    t.float    "Fin_Latitud"
+    t.float    "Fin_Longuitud"
+    t.string   "Frecuencia"
+    t.integer  "Precio"
+    t.string   "Horario"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

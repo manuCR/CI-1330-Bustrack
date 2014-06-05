f6ab82c884bca4e3930a23b33ce40d61c5bea413
Mateo <a33120@ecci.ucr.ac.cr>
2014-06-05 08:57:03 -0600

    Agregar gemas para autenticaciones Crear inicializador para omniauth

--- bustrack/config/routes.rb
+++ bustrack/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :identities
+
   resources :welcomes
   root'welcomes#index'
   resources :paradas
--- bustrack/db/schema.rb
+++ bustrack/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602232154) do
+ActiveRecord::Schema.define(version: 20140605144725) do
+
+  create_table "identities", force: true do |t|
+    t.string   "nombre"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "mapas", force: true do |t|
     t.datetime "created_at"
